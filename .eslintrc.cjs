c45200d46328918e1daa2d90a8a863b8b56cb94e
Debalina Laha <debalinalaha13630@gmail.com>
2024-03-11 20:42:22 +0530

    Made necessary changes in project's dev env and configuration

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -5,11 +5,19 @@ module.exports = {
     'eslint:recommended',
     'plugin:@typescript-eslint/recommended',
     'plugin:react-hooks/recommended',
+    "prettier",
+    'plugin:react/recommended',
+    'plugin:jsx-a11y/recommended',
+    'plugin:prettier/recommended',
   ],
   ignorePatterns: ['dist', '.eslintrc.cjs'],
   parser: '@typescript-eslint/parser',
-  plugins: ['react-refresh'],
+  plugins: [ "prettier",'react','react-refresh','@typescript-eslint','react-hooks','jsx-a11y'],
   rules: {
+    "prettier/prettier": ["error",{
+      "endOfLine": "auto"}
+    ],
+    "prettier/prettier": "error",
     'react-refresh/only-export-components': [
       'warn',
       { allowConstantExport: true },
