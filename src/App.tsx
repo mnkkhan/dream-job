9eba314f7424dc9879594821652efbc62208611b
Debalina Laha <debalinalaha13630@gmail.com>
2024-03-11 21:39:57 +0530

    Implement main application logic and routing

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,12 @@
 import Footer1 from "./components/ui/footer1";
-import Signup from "./signup";
-import CreateProfile from "./createProfile";
+import Signup from "./components/ui/signin";
+import CreateProfile from "./components/ui/createProfile";
 import { Route, Routes} from "react-router-dom";
 function App() {
   return (
     <div>
       <Routes>
-        <Route path="/signup" element={<Signup/>}/>
+        <Route path="/signin" element={<Signup/>}/>
         <Route path="/createProfile" element={<CreateProfile/>}/>
       </Routes>
       <Footer1></Footer1>
