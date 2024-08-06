92f18dbd86c94313dc93ce17a39c6ad53f0115af
mnkkhan <mnkkhan77@gmail.com>
2024-08-06 11:47:53 +0530

    - correction

--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,7 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import { createRoot } from 'react-dom/client';
-import App from './App';
+import { createRoot } from "react-dom/client";
+import App from "./App";
 // import reportWebVitals from './reportWebVitals';
-import { BrowserRouter } from 'react-router-dom'
+import { BrowserRouter } from "react-router-dom";
 const rootElement = document.getElementById("root")!;
 const root = createRoot(rootElement);
 root.render(
