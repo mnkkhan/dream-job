d7b39fb5aa29bec8a5f98170718b641c2cfceebc
Debalina Laha <32841776+Debalina99@users.noreply.github.com>
2024-08-06 11:04:40 +0530

    Made changes in package files

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -5,19 +5,11 @@ module.exports = {
     'eslint:recommended',
     'plugin:@typescript-eslint/recommended',
     'plugin:react-hooks/recommended',
-    "prettier",
-    'plugin:react/recommended',
-    'plugin:jsx-a11y/recommended',
-    'plugin:prettier/recommended',
   ],
   ignorePatterns: ['dist', '.eslintrc.cjs'],
   parser: '@typescript-eslint/parser',
-  plugins: [ "prettier",'react','react-refresh','@typescript-eslint','react-hooks','jsx-a11y'],
+  plugins: ['react-refresh'],
   rules: {
-    "prettier/prettier": ["error",{
-      "endOfLine": "auto"}
-    ],
-    "prettier/prettier": "error",
     'react-refresh/only-export-components': [
       'warn',
       { allowConstantExport: true },
